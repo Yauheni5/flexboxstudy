ab80bbf63e9b6aff9028763d54d1aeb49dbd683b
yauheni5 <yauheni.asheichyk@gmail.com>
2017-01-27 17:30:14 +0300

    add work area

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    browserSync = require('browser-sync');
+
+
+gulp.task('less', function() {
+	return gulp.src('app/less/importer.less')
+	.pipe(less())
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload( {
+		stream:true
+	}))
+});
+
+gulp.task('browserSync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify:false
+	});
+});
+
+gulp.task('watch', ['browserSync', 'less'], function() {
+	 gulp.watch('app/less/*.less', ['less']);
+});
+
